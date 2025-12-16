025f888da9da2c2f24b0216d8e77facf5000c3ad
Yongjia Peng <yopeng@student.42.fr>
2025-12-16 18:28:45 +0100

    update libft

--- src/texture.c
+++ src/texture.c
@@ -5,5 +5,11 @@ bool	parse_texture(int fd, t_data *data, int	*n_line)
 	char	*line;
 
 	line = get_next_line_trim(fd);
-	if ()
+	if (!line)
+		return(false);
+	while (line)
+	{
+		(*n_line)++;
+		
+	}
 }
