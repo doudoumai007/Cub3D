8efa94b0c561a138685d8efc1f05595fd2c09a40
doudoumai007 <lipeiyao_@outlook.com>
2025-12-16 17:43:04 +0100

    add parsing structure

--- include/cub3D.h
+++ include/cub3D.h
@@ -1,14 +1,20 @@
 #ifndef	CUB3D_H
 # define	CUB3D_H
 
+# include <stdbool.h>
+# include <string.h>
+
 # include "parsing.h"
 # include "struct.h"
 # include "../libft/libft.h"
-# include "../minilibx_linux/mlx.h"
+# include "../minilibx-linux/mlx.h"
+
+
 
 # define TEXTURE_NO	0
 # define TEXTURE_SO 1
 # define TEXTURE_WE 2
 # define TEXTURE_EA 3
+
 
 #endif
--- include/parsing.h
+++ include/parsing.h
@@ -1,6 +1,6 @@
 #ifndef PARSING_H
 # define PARSING_H
 
-
+bool	parsing(char *filename, int ac, t_data *data);
 
 #endif
